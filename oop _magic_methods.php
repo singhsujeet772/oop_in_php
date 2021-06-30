a44252e92b60b9edb8c9fcfe92afb4dfc5e3db22
sujeet <singhsujeet772@gmail.com>
2021-06-30 09:09:19 +0530

    oop magic methods

---
+++ oop _magic_methods.php
@@ -0,0 +1,17 @@
+<?php
+    //PHP Magic Methods
+    // __construct()
+    // __destruct()
+    // __get()
+    // __set()
+    // __isset()
+    // __unset()
+    // __autoload()
+    // __clone()
+    // __sleep()
+    // __wakeup()
+    // __call()
+    // __callStatic()
+    // __toString()
+    // __invoke()
+?>
